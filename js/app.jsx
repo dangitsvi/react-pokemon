d51ec70d409eba46bf35b0f84b89df031087f36b
dangitsvi <dangitsvi@gmail.com>
2015-08-23 11:29:59 -0700

    add description feature

--- js/app.jsx
+++ js/app.jsx
@@ -11,12 +11,64 @@ var Header = React.createClass({
   }
 })
 
+var PokemonDescription = React.createClass({
+  getInitialState: function() {
+    return{
+      descriptionURI: '/api/v1/description/4',
+      description: ''
+    }
+  },
+
+  componentWillMount: function () {
+    this.prepareComponentState(this.props);
+  },
+
+  componentWillReceiveProps: function (nextProps) {
+    this.prepareComponentState(nextProps);
+  },
+
+  prepareComponentState: function (props) {
+    //set data on state/template
+    var descriptionURI = props.descriptionURI;
+    request
+      .get('http://pokeapi.co' + descriptionURI)
+      .end(function(err, res) {
+        if (err) return console.log(err);
+        console.log(res);
+        this.setState({
+          descriptionURI: descriptionURI,
+          description: res.body.description
+        })
+      }.bind(this));
+  },
+
+  componentDidMount: function() {
+    request
+      .get('http://pokeapi.co' + this.state.descriptionURI)
+      .end(function(err, res) {
+        if (err) return console.log(err);
+        console.log(res);
+        this.setState({
+          description: res.body.description
+        })
+      }.bind(this));
+  },
+
+  render: function() {
+    return(
+      <div>
+        <p>{this.state.description}</p>
+      </div>
+    )
+  }
+})
+
 var PokemonInfo = React.createClass({
   render: function() {
     return(
       <div>
         <p>Name: {this.props.pokemon}</p>
-        <p>Species: {this.props.species}</p>
+        <PokemonDescription descriptionURI={this.props.descriptionURI} />
         <p>Type 1: {this.props.type1}</p>
         <p>Type 2: {this.props.type2}</p>
       </div>
@@ -30,14 +82,30 @@ var Pokemon = React.createClass({
     request
       .get('http://pokeapi.co/api/v1/pokemon/' + (this.state.index +1))
       .end(function(err, res) {
+        if (err) return console.log(err);
         this.setState({
           index: this.state.index + 1,
           pokemon: res.body.name,
           species: res.body.species,
-          types: res.body.types
+          types: res.body.types,
+          descriptionURI: res.body.descriptions[0].resource_uri
         })
       }.bind(this))
+  },
 
+  decrementCount: function(pokemon) {
+    request
+      .get('http://pokeapi.co/api/v1/pokemon/' + (this.state.index -1))
+      .end(function(err, res) {
+        if (err) return console.log(err);
+        this.setState({
+          index: this.state.index - 1,
+          pokemon: res.body.name,
+          species: res.body.species,
+          types: res.body.types,
+          descriptionURI: res.body.descriptions[0].resource_uri
+        })
+      }.bind(this))
   },
 
   getInitialState: function() {
@@ -45,7 +113,8 @@ var Pokemon = React.createClass({
       index: 1,
       pokemon:'',
       species:'',
-      types:[{name:""},{name:""}]
+      types:[{name:""},{name:""}],
+      descriptionURI:''
     }
   },
 
@@ -53,11 +122,12 @@ var Pokemon = React.createClass({
     request
       .get('http://pokeapi.co/api/v1/pokemon/' + this.state.index)
       .end(function(err, res) {
+        if (err) return console.log(err);
         this.setState({
           pokemon: res.body.name,
           species: res.body.species,
-          types: res.body.types
-
+          types: res.body.types,
+          descriptionURI: res.body.descriptions[0].resource_uri
         })
       }.bind(this))
   },
@@ -65,12 +135,14 @@ var Pokemon = React.createClass({
   render: function() {
     return (
       <div>
-
         <img src={"http://pokeapi.co/media/img/" + this.state.index + ".png"} />
-
-        <PokemonInfo pokemon={this.state.pokemon} species={this.state.species} type1={this.state.types[0].name} type2={this.state.types[1] ? this.state.types[1].name:"None"} />
-
-        <button>prev</button>
+        <PokemonInfo
+          pokemon={this.state.pokemon}
+          species={this.state.species}
+          type1={this.state.types[0].name}
+          type2={this.state.types[1] ? this.state.types[1].name:"None"}
+          descriptionURI={this.state.descriptionURI} />
+        <button onClick={this.decrementCount}>prev</button>
         <button onClick={this.incrementCount}>next</button>
       </div>
     )
@@ -81,7 +153,6 @@ var Pokedex = React.createClass({
   render: function() {
     return(
       <section>
-        <p>This is the pokedex</p>
         <Pokemon />
       </section>
     )
